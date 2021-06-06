e475eb27fa348a3c470e0a793c371bd9601fc5d8
gupta, kapil <kapil.erg@gmail.com>
2021-06-06 13:21:16 +0530

    back

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const postsRoutes = require("./routes/posts");
-const userRoutes = require("./routes/user");
+
+const Post = require("./models/post")
+
 const app = express();
-
-mongoose
-  .connect(
-    "mongodb+srv://kapil:dm4iEl0AnWm69zpM@meancluster.77lim.mongodb.net/my-FirstDB?retryWrites=true&w=majority"
-  )
+mongoose.connect(
+    "mongodb+srv://kapil:dm4iEl0AnWm69zpM@meancluster.77lim.mongodb.net/my-FirstDB?retryWrites=true"
+      )
   .then(() => {
-    console.log("Connected to database!");
+    console.log("Connected to Database!");
   })
   .catch(() => {
-    console.log("Connection failed!");
+    console.log("Connection Failed!");
   });
 
 app.use(bodyParser.json());
@@ -24,15 +23,39 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    "GET, POST, PATCH, DELETE, OPTIONS"
   );
   next();
 });
 
-app.use("/api/posts", postsRoutes);
-app.use("/api/user", userRoutes);
+app.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save();
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
+});
+
+app.get("/api/posts", (req, res, next) => {
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents,
+    });
+  });
+
+});
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then((result) => {
+    console.log(result);
+    res.status(200).json({ message: "Post deleted!" });
+  });
+});
 module.exports = app;
